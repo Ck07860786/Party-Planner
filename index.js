482ede66a04625b48814ec3be60913a7c10137a6
Chotu Kumar <ck4001252@gmail.com>
2024-06-07 19:42:59 +0530

    some chage in index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/api/v1/category', categoryRoute);
 app.use('/api/v1/service', serviceRoutes);
 app.use('/api/v1/review', reviewRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
