7f145e2aea492e9b28249638e8f17ed08127df2f
Chotu Kumar <ck4001252@gmail.com>
2024-06-09 16:37:39 +0530

    few changes

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,10 @@
+import cloudinary from 'cloudinary';
+          
+cloudinary.v2.config({ 
+  cloud_name: 'ddzvtasy8', 
+  api_key: '696733841292532', 
+  api_secret: 'viUf0KCPJdx1e1WWfK3fNpGIQ5A', 
+  
+});
+
+export default cloudinary;
