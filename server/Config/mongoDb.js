903fd3ed68c1ce8e58edb9e5e07d6f1cf7748ba2
Chotu Kumar <ck4001252@gmail.com>
2024-06-14 08:40:18 +0530

    some database settings chnged

--- server/Config/mongoDb.js
+++ server/Config/mongoDb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const mongoConnect = async()=>{
+const mongoConnect = async(req,res)=>{
     const connect = await mongoose.connect(process.env.MONGO_URL)
     console.log(`database Connected successfully ${connect.connection.host}`)
     try {
